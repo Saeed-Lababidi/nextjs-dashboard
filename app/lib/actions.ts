6ed9a5f05ab41cd21f5ab277379dc4170781cc09
Saeed-Lababidi <69202298+Saeed-Lababidi@users.noreply.github.com>
2025-02-09 16:51:29 +0300

    added authentication using NextAuth.js. Also added middleware.ts to protect the routes with password hashing and a logout button

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,9 +4,27 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { sql } from '@vercel/postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
-// They are wrong here for some reason, I added the above from data.ts
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
   id: z.string(),
